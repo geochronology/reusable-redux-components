e187165239615abb98c84d13c3791cd41c26fc89
geochronology <54387644+geochronology@users.noreply.github.com>
2020-11-24 21:54:00 -0800

    initialized project, added file tree, created page components

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HomePageComponent = () => {
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+    </div>
+  )
+}
---
+++ src/pages/ResultsPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ResultsPage = () => {
+  return (
+    <div className="container">
+      <h1>Results Page</h1>
+    </div>
+  )
+}
